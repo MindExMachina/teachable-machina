c15d44f66062afcec8c77d7b69fa549374cf37ff
Nono Martínez Alonso <mundowarezweb@gmail.com>
2018-05-06 16:09:07 -0400

    Push teachable-machina 180506.

--- README.md
+++ README.md
@@ -1,1 +1,44 @@
-# teachable-machina
+# Teachable Machina
+
+![Teachable Machina](assets/static/cover.jpg)
+
+Teachable Machina is an experiment that makes it easier for anyone to explore machine learning, live in the browser – no coding required — and send instructions to Machina bridge to be executed by a robot.
+
+Learn more about the original experiment and try it yourself on [g.co/teachablemachine](https://g.co/teachablemachine). The experiment is built using the [deeplearn.js](https://github.com/PAIR-code/deeplearnjs) library.
+
+There is also released a boilerplate version of this project that can be used as a starting point for your own projects: [googlecreativelab/teachable-machine-boilerplate](https://github.com/googlecreativelab/teachable-machine-boilerplate)
+
+## Development
+#### Install dependencies by running (similar to `npm install`)
+```
+yarn
+```
+
+#### Build project
+```
+yarn build
+```
+
+#### Start local server by running 
+```
+yarn run watch
+```
+
+#### Code Styles
+- There’s a pre-commit hook set up that will prevent commits when there are errors
+- Run `yarn eslint` for es6 errors & warnings
+- Run `yarn stylint` for stylus errors & warnings
+
+#### To run https locally:
+https is required to get camera permissions to work when not working with `localhost`
+
+1. Generate Keys
+```
+openssl genrsa -out server.key 2048
+openssl req -new -x509 -sha256 -key server.key -out server.cer -days 365 -subj /CN=YOUR_IP
+```
+2. Use `yarn run watch-https`
+3. Go to `https://YOUR_IP:3000`, then accept the insecure privacy notice, and proceed.
+
+## Credit
+This is not an official Google product, but an experiment that was a collaborative effort by friends from [Støj](http://stoj.io/), [Use All Five](https://useallfive.com/) and Creative Lab and [PAIR](https://ai.google/pair/) teams at Google.
---
+++ src/config.js
@@ -0,0 +1,233 @@
+import Robot from "./sg/machina";
+
+// Copyright 2017 Google Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+let AudioContext = window.AudioContext || window.webkitAudioContext;
+let GLOBALS = {
+    // WS config
+    ws: undefined,
+    wsHost: '127.0.0.1',
+    wsPort: '6999',
+    wsPath: 'Bridge',
+    currentId: '',
+    currentIdDuration: 0,
+    currentIdTriggerAt: 2,  // defines how many prediction ticks are needed to trigger a classHandler request
+
+    // Robot stuff
+    robot: {
+        driver: undefined,
+        make: "UR",
+
+        initialize: function(socket) { 
+            this.driver = new Robot(socket);
+
+            // Override any previou tool info
+            this.driver.Tool("noTool", 0, 0, 0, 1, 0, 0, 0, 1, 0, 0.1, 0, 0, 0);
+            this.driver.Attach("noTool");
+    
+            this.driver.Message(new Date(Date.now()));
+            this.driver.Message("Starting Teachable Machina");
+            this.driver.SpeedTo(100);
+            this.driver.MotionMode("joint");
+            switch(this.make.toUpperCase()) {
+                case "ABB":
+                    this.driver.AxesTo(0, 0, 0, 0, 90, 0);
+                    // As Machina still doesn't support FK/IK, we need to set TCP values after an Axes movement to be able to issue relative actions afterwards
+                    this.driver.TransformTo(451, 0, 807, -1, 0, 0, 0, 1, 0);  // AxesTo(0, 0, 0, 0, 90, 0) for a IRB1200
+                    break; 
+                case "UR":
+                    this.driver.AxesTo(0, -90, -90, -90, 90, 90);
+                    // As Machina still doesn't support FK/IK, we need to set TCP values after an Axes movement to be able to issue relative actions afterwards
+                    this.driver.TransformTo(688, -158, 647, -1, 0, 0, 0, 1, 0);  // AxesTo(0, -90, -90, -90, 90, 90) for an UR10
+
+                default:
+                    this.driver.Message("Cannot home this robot");
+                    break;
+            }
+            this.driver.Rotate(0, 1, 0, -90);
+        },
+        onMessage: function (msg) {
+            let obj = JSON.parse(msg);
+            //$"{{\"msg\":\"action-completed\",\"data\":[{e.RemainingActions}]}}");
+            if (obj.msg == 'action-completed') {
+                if (obj.data[0] < 1) {
+                    if (GLOBALS.currentInstruction != 0) {
+                        this.sendMotionInstruction();
+                    } 
+                    else 
+                    {
+                        this.isStopped = true;
+                    }
+                }
+            }
+        },
+        sendMotionInstruction: function() {
+            console.log("sending instruction");
+            switch(GLOBALS.currentInstruction) {
+                case 1:
+                    this.driver.Move(0, 0, this.displacement);
+                    break;
+
+                case 2:
+                    this.driver.Move(0, this.displacement, 0);
+                    break;
+
+                case 3:
+                    this.driver.Move(0, 0, -this.displacement);
+                    break;
+
+                case 4: 
+                    this.driver.Move(0, -this.displacement, 0);
+                    break;
+            }
+            this.isStopped = false;
+        },
+        displacement: 10,
+        isStopped: true,
+    },
+
+    numClasses: 5,
+    classNames: [
+        'green',
+        'purple',
+        'orange',
+        'red',
+        'blue',
+        // 'yellow',
+        // 'brown'
+    ],
+    colors: {
+        'green': '#2baa5e',
+        'purple': '#c95ac5',
+        'orange': '#f28621',
+        'red': '#e8453c',
+        'blue': '#4286f4',
+        // 'yellow': '#4286f4',
+        // 'brown': '#4286f4'
+    },
+    rgbaColors: {
+        'green': 'rgba(43, 170, 94, 0.25)',
+        'purple': 'rgba(201, 90, 197, 0.25)',
+        'orange': 'rgba(221, 77, 49, 0.25)',
+        'red': 'rgba(232, 69, 60, 0.25)',
+        'blue': 'rgba(66, 134, 244, 1)',
+        // 'yellow': 'rgba(66, 134, 244, 1)',
+        // 'brown': 'rgba(66, 134, 244, 1)'
+    },
+    classesTrained: {
+        'green': false,
+        'purple': false,
+        'orange': false,
+        'red': false,
+        'blue': false,
+        // 'yellow': false,
+        // 'brown': false
+    },
+    classImages: {
+        'green': 'assets/classImages/green.png',
+        'purple': 'assets/classImages/purple.png',
+        'orange': 'assets/classImages/orange.png',
+        'red': 'assets/classImages/red.png',
+        'blue': 'assets/classImages/blue.png',
+        // 'yellow': 'blue.png',
+        // 'brown': 'blue.png'
+    },
+
+    currentInstruction: 0,
+    classHandlers: {
+
+        // What happens when the classified id is "green"?
+        "green": function(id) {
+            console.log('This executes when ' + id + ' activates.');
+            console.log('Verde');
+            // arm.Message("STOP");
+            GLOBALS.currentInstruction = 0;  // STOP
+        },
+
+        // or "purple"
+        "purple": function(id) {
+            console.log('This executes when ' + id + ' activates.');
+            console.log('Morado.');
+            // arm.Message("UP");
+            // arm.Move(0, 0, GLOBALS.robotDisplacement);
+            GLOBALS.currentInstruction = 1;  // UP
+            if (GLOBALS.robot.isStopped) GLOBALS.robot.sendMotionInstruction();
+        },
+
+        // or "orange"
+        "orange": function(id) {
+            console.log('This executes when ' + id + ' activates.');
+            console.log('Naranja.');
+            // arm.Message("RIGHT");
+            // arm.Move(0, GLOBALS.robotDisplacement, 0);
+            GLOBALS.currentInstruction = 2;  // RIGHT
+            if (GLOBALS.robot.isStopped) GLOBALS.robot.sendMotionInstruction();
+        },
+
+        "red": function(id) {
+            console.log('This executes when ' + id + ' activates.');
+            console.log('Rojo.');
+            // arm.Message("DOWN");
+            // arm.Move(0, 0, -GLOBALS.robotDisplacement);
+            GLOBALS.currentInstruction = 3;  // DOWN
+            if (GLOBALS.robot.isStopped) GLOBALS.robot.sendMotionInstruction();
+        },
+
+        "blue": function(id) {
+            console.log('This executes when ' + id + ' activates.');
+            console.log('Naranja.');
+            // arm.Message("LEFT");
+            // arm.Move(0, -GLOBALS.robotDisplacement, 0);
+            GLOBALS.currentInstruction = 4;  // LEFT
+            if (GLOBALS.robot.isStopped) GLOBALS.robot.sendMotionInstruction();
+        },
+        
+        
+        // If no handler is provided for a class id, this gets executed.
+
+        "default": function(id) {
+
+            console.log('Default class handler. No handler provided for class ' + id + '.');
+
+            // GLOBALS.ws.send('{"method":"send-message", "params": {"text": "This is Teachable Machine here: ' + id + '"}}');
+            // GLOBALS.ws.send('{"method":"set-background-color", "params": {"color": "' + GLOBALS.colors[id] + '"}}');
+
+            GLOBALS.currentInstruction = 0;  // STOP
+        }
+    },
+
+    /* end · sg edit*/
+    button: {
+        /* start · sg edit*/
+        padding: 0,
+        frontHeight: 40,
+        states: {
+            normal: {
+                x: 8,
+                y: 8
+            },
+            pressed: {
+                x: 4,
+                y: 4
+            }
+        }
+    },
+    classId: null,
+    predicting: false,
+    micThreshold: 25,
+    audioContext: new AudioContext(),
+    isBackFacingCam: false
+};
+
+export default GLOBALS;
